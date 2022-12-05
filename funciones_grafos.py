701c9f0759c878a90e8f25a081369c2e3d97303d
Fran <fdelucaa@fi.uba.ar>
2022-12-05 15:21:49 -0300

    agrego peso a fleury y hierholzer

--- funciones_grafos.py
+++ funciones_grafos.py
@@ -146,56 +146,54 @@ def vertices_grado_impar(grafo):
 #   Ciclo   #
 
 def ciclo_euleriano(grafo, origen):
-    ciclo = [] 
-    if es_conexo(grafo) and vertices_grado_impar(grafo) == 0:
-        ciclo = hierholzer(grafo, origen)
-    elif es_conexo and vertices_grado_impar(grafo) == 2:
-        return ciclo
+    ciclo = []
+    peso_ciclo = 0 
+    if es_conexo(grafo):
+        impares = vertices_grado_impar(grafo)
+        if impares == 0:
+            ciclo, peso_ciclo = hierholzer(grafo, origen)
+        elif impares == 2:
+            ciclo, peso_ciclo = fleury(grafo)
+        return ciclo, peso_ciclo
     else:
-        return
-    return ciclo
+        return None, None
 
 #            HIERHOLZER            #
 
 def hierholzer(grafo, origen):
     camino = []
     aristas_visit = {}
+    peso = [0]
     camino.append(origen)
-    dfs_hierholzer(grafo, origen, aristas_visit, camino)
+    dfs_hierholzer(grafo, origen, aristas_visit, camino, peso)
     i = 0
     while i < len(camino):
         nuevo_camino = [camino[i]]
-        if not dfs_hierholzer(grafo, camino[i], aristas_visit, nuevo_camino):
+        if not dfs_hierholzer(grafo, camino[i], aristas_visit, nuevo_camino, peso):
             i += 1
             continue
-        
-        # ej: i = 2              |
-        # camino:       [v1, v2, v3, v4, v5, v6]
-        # nuevo_camino:           \ [v3, v8, v9, v3]
         camino = camino[:i] + nuevo_camino + camino[i+1:]
-    return camino
+    return camino, peso[0]
 
-def dfs_hierholzer(grafo, v, aristas_visit, camino):
-    """
-    Encuentra un ciclo que comienza en v y termina en v sin utilizar las aristas
-    visitadas. Devuelve True si se encontró o False si no
-    """ 
-    return _dfs_hierholzer(grafo, v, aristas_visit, camino, v)
+def dfs_hierholzer(grafo, v, aristas_visit, camino, peso):
+    return _dfs_hierholzer(grafo, v, aristas_visit, camino, v, peso)
     
-def _dfs_hierholzer(grafo, v, aristas_visit, camino, origen):
+def _dfs_hierholzer(grafo: Grafo, v, aristas_visit, camino, origen, peso):
     adyacentes_v: set = aristas_visit.setdefault(v, set())
     aristas_adyacentes_sin_visitar = set(grafo.adyacentes(v)).difference(adyacentes_v)
     for w in aristas_adyacentes_sin_visitar:
         adyacentes_w: set = aristas_visit.setdefault(w, set())
         adyacentes_v.add(w)
         adyacentes_w.add(v)
+        peso[0] += grafo.peso(v,w)
 
         camino.append(w)
-        if w == origen or _dfs_hierholzer(grafo, w, aristas_visit, camino, origen):
+        if w == origen or _dfs_hierholzer(grafo, w, aristas_visit, camino, origen, peso):
             return True
         
         adyacentes_v.remove(w)
         adyacentes_w.remove(v)
+        peso[0] -= grafo.peso(v,w)
         camino.pop()
 
     return False
@@ -204,25 +202,26 @@ def _dfs_hierholzer(grafo, v, aristas_visit, camino, origen):
 
 def fleury(grafo):
     v = vertice_grado_impar(grafo)
-    aristas = obtener_aristas(grafo)
     grafo_aux = grafo
     aristas_visit = set()
     camino = []
     camino.append(v)
-    dfs_fleury(grafo_aux, v, aristas_visit, camino)
-    return camino
+    peso = [0]
+    dfs_fleury(grafo_aux, v, aristas_visit, camino, peso)
+    return camino, peso[0]
     
-def dfs_fleury(grafo, v, aristas_visit, camino):
+def dfs_fleury(grafo: Grafo, v, aristas_visit, camino, peso):
     for w in grafo.adyacentes(v):
         if (v,w) not in aristas_visit:
             grafo.eliminar_arista(v,w)
             if not es_conexo(grafo):
-                dfs_fleury(grafo,v,aristas_visit,camino)
+                dfs_fleury(grafo,v, aristas_visit, camino, peso)
             grafo.agregar_arista(v,w)
             camino.append(w)
             aristas_visit.add((w,v))
             aristas_visit.add((v,w))
-            dfs_fleury(grafo,w,aristas_visit,camino)
+            peso[0] += grafo.peso(v,w)
+            dfs_fleury(grafo, w, aristas_visit, camino, peso)
 
 
 # Auxiliares #
@@ -234,8 +233,6 @@ def vertice_grado_impar(grafo):
             return v
     return None
         
-    
-
 def obtener_aristas(grafo):
     aristas = []
     visitados = set()
@@ -243,6 +240,6 @@ def obtener_aristas(grafo):
         for w in grafo.adyacentes(v):
             if w not in visitados:
                 aristas.append((v,w))
-            visitados.add(v)
+        visitados.add(v)
     return aristas
 
--- pruebadeciclos.py
+++ pruebadeciclos.py
@@ -6,36 +6,24 @@ from funciones_grafos import fleury
 
 
 ##      PARA HIERHOLZER
-#def agregar_aristas(grafo: Grafo):
-#    grafo.agregar_arista("Arg", "Uru")
-#    grafo.agregar_arista("Arg", "Bra")
-#    grafo.agregar_arista("Arg", "Par")
-#    grafo.agregar_arista("Arg", "Chil")
-#    grafo.agregar_arista("Arg", "Peru")
-#    grafo.agregar_arista("Arg", "Ecua")
-#    grafo.agregar_arista("Uru", "Bra")
-#    grafo.agregar_arista("Uru", "Boliv")
-#    grafo.agregar_arista("Uru", "Par")
-#    grafo.agregar_arista("Bra", "Boliv")
-#    grafo.agregar_arista("Bra", "Chil")
-#    grafo.agregar_arista("Chil", "Peru")
-#    grafo.agregar_arista("Chil", "Ecua")
-#    grafo.agregar_arista("Par", "Peru")
-#    grafo.agregar_arista("Par", "Ecua")
-#    grafo.agregar_arista("Ecua", "Peru")
-#    
-#def agregar_vertices(grafo: Grafo):
-#    grafo.agregar_vertice("Arg")
-#    grafo.agregar_vertice("Uru")
-#    grafo.agregar_vertice("Bra")
-#    grafo.agregar_vertice("Par")
-#    grafo.agregar_vertice("Chil")
-#    grafo.agregar_vertice("Peru")
-#    grafo.agregar_vertice("Ecua")
-#    grafo.agregar_vertice("Boliv")
+def agregar_aristas(grafo: Grafo):
+    grafo.agregar_arista("Arg", "Uru")
+    grafo.agregar_arista("Arg", "Bra")
+    grafo.agregar_arista("Arg", "Par")
+    grafo.agregar_arista("Arg", "Chil")
+    grafo.agregar_arista("Arg", "Peru")
+    grafo.agregar_arista("Arg", "Ecua")
+    grafo.agregar_arista("Uru", "Bra")
+    grafo.agregar_arista("Uru", "Boliv")
+    grafo.agregar_arista("Uru", "Par")
+    grafo.agregar_arista("Bra", "Boliv")
+    grafo.agregar_arista("Bra", "Chil")
+    grafo.agregar_arista("Chil", "Peru")
+    grafo.agregar_arista("Chil", "Ecua")
+    grafo.agregar_arista("Par", "Peru")
+    grafo.agregar_arista("Par", "Ecua")
+    grafo.agregar_arista("Ecua", "Peru")
 
-
-##      PARA FLEURY
 def agregar_vertices(grafo: Grafo):
     grafo.agregar_vertice("Arg")
     grafo.agregar_vertice("Uru")
@@ -43,21 +31,30 @@ def agregar_vertices(grafo: Grafo):
     grafo.agregar_vertice("Par")
     grafo.agregar_vertice("Chil")
     grafo.agregar_vertice("Peru")
-    
-    
-def agregar_aristas(grafo: Grafo):
-    grafo.agregar_arista("Arg", "Uru")
-    grafo.agregar_arista("Arg", "Bra")
-    grafo.agregar_arista("Arg", "Par")
-    grafo.agregar_arista("Arg", "Chil")
-    grafo.agregar_arista("Uru", "Bra")
-    grafo.agregar_arista("Uru", "Par")
-    grafo.agregar_arista("Uru", "Peru")
-    grafo.agregar_arista("Bra", "Chil")
-    grafo.agregar_arista("Bra", "Peru")
-    grafo.agregar_arista("Chil", "Par")
-    
-
+    grafo.agregar_vertice("Ecua")
+    grafo.agregar_vertice("Boliv")
+##   #   PARA FLEURY
+#def agregar_vertices(grafo: Grafo):
+#    grafo.agregar_vertice("Arg")
+#    grafo.agregar_vertice("Uru")
+#    grafo.agregar_vertice("Bra")
+#    grafo.agregar_vertice("Par")
+#    grafo.agregar_vertice("Chil")
+#    grafo.agregar_vertice("Peru")
+#    
+#    
+#def agregar_aristas(grafo: Grafo):
+#    grafo.agregar_arista("Arg", "Uru")
+#    grafo.agregar_arista("Arg", "Bra")
+#    grafo.agregar_arista("Arg", "Par")
+#    grafo.agregar_arista("Arg", "Chil")
+#    grafo.agregar_arista("Uru", "Bra")
+#    grafo.agregar_arista("Uru", "Par")
+#    grafo.agregar_arista("Uru", "Peru")
+#    grafo.agregar_arista("Bra", "Chil")
+#    grafo.agregar_arista("Bra", "Peru")
+#    grafo.agregar_arista("Chil", "Par")
+#    
 def crear_grafo():
     g = Grafo(False, [])
     agregar_vertices(g)
@@ -66,8 +63,9 @@ def crear_grafo():
     
 def main():
     g = crear_grafo()
-    #camino = hierholzer(g)
-    camino = fleury(g)
+    camino, peso = hierholzer(g, "Arg")
+    #camino, peso = fleury(g)
     print(camino)
+    print(peso)
     
 main()
